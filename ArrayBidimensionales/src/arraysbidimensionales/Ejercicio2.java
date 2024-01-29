3f62b28c24562b864e32773466441c927e903783
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-01-29 20:41:57 +0100

    Ejercicio

--- ArrayBidimensionales/src/arraysbidimensionales/Ejercicio2.java
+++ ArrayBidimensionales/src/arraysbidimensionales/Ejercicio2.java
@@ -12,20 +12,14 @@ public class Ejercicio2 {
 
 		int nota;
 
-		int contadorN = 0;
-		int contadorA = 0;
-
-		for (int i = 0; i < 4; i++) {
-			contadorN++;
-			for (int j = 0; i < 5; j++) {
-				contadorA++;
-				System.out.println("Introduce la nota " + contadorA + " del alumno " + contadorN + " --> ");
+		for (int i = 1; i <5; i++) {
+			for (int j = 1; i < 6; j++) {
+				System.out.println("Introduce la nota " + j + " del alumno " + i + " --> ");
 				nota = sc.nextInt();
 				tabla[i][j] = nota;
 			}
 		}
 
-		
 		for (int[] fila : tabla) {
 			for (int valor : fila) {
 				System.out.print(valor + "\t");
