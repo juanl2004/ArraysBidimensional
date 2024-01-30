a7fc0a0eca51ac43c26c98d8beb4f3ce2f929d29
jlbarrionuevo <jlbarrionuevo@106-06.info2>
2024-01-30 14:39:34 +0100

    Ejercicios

--- ArrayBidimensionales/src/arraysbidimensionales/Ejercicio2.java
+++ ArrayBidimensionales/src/arraysbidimensionales/Ejercicio2.java
@@ -1,5 +1,6 @@
 package arraysbidimensionales;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio2 {
@@ -8,23 +9,36 @@ public class Ejercicio2 {
 
 		Scanner sc = new Scanner(System.in);
 
-		int tabla[][] = new int[4][5];
+		int[][] tabla = new int[4][5];
 
-		int nota;
-
-		for (int i = 1; i <5; i++) {
-			for (int j = 1; i < 6; j++) {
-				System.out.println("Introduce la nota " + j + " del alumno " + i + " --> ");
-				nota = sc.nextInt();
-				tabla[i][j] = nota;
+		for (int fila = 0; fila < 4; fila++) {
+			for (int columna = 0; columna < 5; columna++) {
+				System.out.println("Introduce la nota " + (columna + 1) + " del alumno " + (fila + 1) + " --> ");
+				tabla[fila][columna] = sc.nextInt();
 			}
 		}
+		
 
 		for (int[] fila : tabla) {
 			for (int valor : fila) {
 				System.out.print(valor + "\t");
+				
 			}
 			System.out.println();
+		}
+
+		int notaMin = 0;
+		int notaMax = 0;
+
+		for (int fila = 0; fila < 4; fila++) {
+			System.out.println();
+			Arrays.sort(tabla[fila]);
+			notaMin = tabla[fila][0];
+			notaMax = tabla[fila][4];
+			System.out.println("Alumno " + (fila + 1));
+			System.out.println("Nota minima --> " + notaMin);
+			System.out.println("Nota maxima --> " + notaMax);
+
 		}
 
 		sc.close();
