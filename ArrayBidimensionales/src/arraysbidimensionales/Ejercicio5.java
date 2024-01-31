6bf72567e34ee8781172d00c88ca2d09d73d1acb
jlbarrionuevo <jlbarrionuevo@106-06.info2>
2024-01-31 14:30:55 +0100

    Ejercicios

--- ArrayBidimensionales/src/arraysbidimensionales/Ejercicio5.java
+++ ArrayBidimensionales/src/arraysbidimensionales/Ejercicio5.java
@@ -8,39 +8,46 @@ public class Ejercicio5 {
 
 		int[][] resultado = Ejercicio5.tablaAletorios();
 
-       
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(resultado[i][j] + "\t");
-            }
-            System.out.println();
-        }
+		int sumaFilas = 0;
+		int sumaColumna = 0;
+		int total = 0;
+
+		for (int[] fila : resultado) {
+			sumaFilas = 0;
+
+			for (int valor : fila) {
+				System.out.print(valor + "\t");
+				sumaFilas += valor;
+			}
+			System.out.println(sumaFilas);
+		}
+		
+		for (int j=0; j<resultado[0].length; j++) {
+			sumaColumna = 0;
+			for(int i=0; i<resultado.length; i++) {
+				sumaColumna += resultado[i][j];
+			}
+			total += sumaColumna;
+			System.out.print(sumaColumna + "\t");
+		}
+		System.out.println(total);
+
 	}
 
 	public static int[][] tablaAletorios() {
 
 		Random random = new Random();
 
-		int[][] Aleatorio = new int[5][6];
+		int[][] aleatorio = new int[4][5];
 
-		int suma = 0;
-		
-		for (int i = 0; i < 4; i++) {
-			
-			suma = 0;
-			for (int j = 0; j < 6; j++) {
-				if (j==5) {
-					
-					Aleatorio[5][i] = suma;
-				}
-				Aleatorio[i][j] = random.nextInt(900) + 100;
-				suma += Aleatorio[i][j];
-				
+		for (int i = 0; i < aleatorio.length; i++) {
+
+			for (int j = 0; j < aleatorio.length + 1; j++) {
+				aleatorio[i][j] = random.nextInt(100, 1000);
 			}
-			
 		}
 
-		return Aleatorio;
+		return aleatorio;
 
 	}
 }
